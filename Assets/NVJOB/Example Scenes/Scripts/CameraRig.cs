909696909eeab3361b66bad6549cdf71450f1dc9
#NVJOB Nicholas Veselov <reset86@gmail.com>
2019-09-13 02:42:51 +0300

    v2.2

---
+++ Assets/NVJOB/Example Scenes/Scripts/CameraRig.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class CameraRig : MonoBehaviour
+{
+    public float rot = 1;
+    public Transform ConditionalPlayer;
+
+    Transform tr;
+    float mouseV, mouseH;
+    Vector3 startRot;
+
+    void Awake()
+    {
+        tr = transform;
+        startRot = ConditionalPlayer.localEulerAngles;
+    }
+
+    void Update()
+    {
+        tr.Rotate(Vector3.up, rot * Time.deltaTime);
+
+        if (Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.5f)
+        {
+            mouseH += 0.4f * Input.GetAxis("Mouse X");
+            mouseV -= 0.4f * Input.GetAxis("Mouse Y");
+        }
+        ConditionalPlayer.localEulerAngles = startRot + new Vector3(Mathf.Clamp(mouseV, -20, 20), mouseH, 0);
+    }
+}
