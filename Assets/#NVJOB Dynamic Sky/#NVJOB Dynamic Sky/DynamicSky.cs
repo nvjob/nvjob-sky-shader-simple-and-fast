f6108ca082912ea000f2544628ed4c8c31ed28c0
#NVJOB Nicholas Veselov <reset86@gmail.com>
2020-05-27 11:39:49 +0300

    v2.5

---
+++ Assets/#NVJOB Dynamic Sky/#NVJOB Dynamic Sky/DynamicSky.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2016 Unity Technologies. MIT license - license_unity.txt
+// #NVJOB Dynamic Sky. MIT license - license_nvjob.txt
+// #NVJOB Dynamic Sky v2.5 - https://nvjob.github.io/unity/nvjob-dynamic-sky-lite
+// #NVJOB Nicholas Veselov - https://nvjob.github.io
+
+
+using UnityEngine;
+
+[HelpURL("https://nvjob.github.io/unity/nvjob-dynamic-sky-lite")]
+[AddComponentMenu("#NVJOB/Dynamic Sky")]
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+[ExecuteInEditMode]
+public class DynamicSky : MonoBehaviour
+{
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public float ssgUvRotateSpeed = 1;
+    public float ssgUvRotateDistance = 1;
+    public Transform player;
+
+    //--------------
+
+    Vector2 ssgVector;
+    Transform tr;
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private void Awake()
+    {
+        //--------------
+
+        ssgVector = Vector2.zero;
+        tr = transform;
+
+        //--------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    void Update()
+    {
+        //--------------
+
+        ssgVector = Quaternion.AngleAxis(Time.time * ssgUvRotateSpeed, Vector3.forward) * Vector2.one * ssgUvRotateDistance;
+        Shader.SetGlobalFloat("_SkyShaderUvX", ssgVector.x);
+        Shader.SetGlobalFloat("_SkyShaderUvZ", ssgVector.y);
+
+        //--------------
+
+        if (player != null) tr.position = new Vector3(player.position.x, tr.position.y, player.position.z);
+
+        //--------------
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
